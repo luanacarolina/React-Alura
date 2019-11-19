34560dc3f49dcf19e779c025de578ab1094767bd
luanacarolina <luanalcj1@hotmail.com>
2019-11-19 16:54:20 -0200

    inserção de arquivos remove nos states

--- project/react-alura/src/App.js
+++ project/react-alura/src/App.js
@@ -40,6 +40,32 @@ class App extends Component {
   
     ],
   };
+  //Método que remove um elemento do array autores.
+   removeAutor = index =>{
+     //constante autores recebendo this.state.
+    const {autores} = this.state;
+    
+    //Como não é possível alterar um state diretamente, teremos 
+    //que utilizar um método específico do React para efetuar 
+    //essa alteração, chamado setState(). Esse método recebe um JSON
+    // que servirá para fazer o merge dos dados que já estão no 
+    //state e aqueles que estão sendo passados
+    this.setState(
+      {
+        //chamaremos o método filter(), dentro do qual escreveremos
+        // um método que recebe o autor (a unidade dentro de autores) e a 
+        //posAtual dessa iteração.
+        autores:autores.filter((autor,posAtual)=>{
+          console.log(index,posAtual);
+
+          //Exclui  um autor quando a posição
+          // da iteração atual for igual ao index sendo recebido
+          return posAtual!==index;
+        }),
+      }
+    );
+   }
+
   //Componente criado por meio de uma classe precisa 
   //obrigatoriamente , da declaração do método render().
   //Passamos o retorno para dentro desse metodo.
@@ -48,7 +74,7 @@ class App extends Component {
     //this.state.autores , acessa a chave autores por meio do state
     <div className="App">
       
-     <Tabela autores = {this.state.autores}/>
+     <Tabela autores = {this.state.autores} removeAutor = {this.removeAutor}/>
     </div>
   );
 }
